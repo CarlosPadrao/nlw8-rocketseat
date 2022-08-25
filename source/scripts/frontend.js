7997e4266030fe90d16229c8372d5ae44959b5af
carlos_padrao <carlospadrao@baroes.com.br>
2022-08-25 00:07:58 -0300

    try-to-go-v2

--- source/scripts/frontend.js
+++ source/scripts/frontend.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+});
+//
 var openModal = document.querySelector('#open-modal');
 var closeModal = document.querySelector('#close-modal');
 var mobileModal = document.querySelector('#mobile-modal')
@@ -15,41 +40,16 @@ closeModal.addEventListener('click', function () {
     headerMob.classList.remove('d-none');
 });
 
-
-// target link close Modal
-var itemNavMobile = document.querySelectorAll('.item-nav-mobile');
-itemNavMobile.addEventListener("click", esconder);
-
+//
 function esconder() {
     mobileModal.style.display = 'none';
     headerMob.classList.remove('d-none');
 }
 
+// target link close Modal
+var navMobile = document.querySelectorAll('.item-nav-mobile');
+
+// alert message
 function alerta() {
     window.alert('Em desenvolvimento, volte mais tarde! :D');
 }
-//
-$(document).ready(function(){
-// Add smooth scrolling to all links
-$("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-    });
-    } // End if
-});
-});
